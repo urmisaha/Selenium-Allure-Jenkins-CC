1431904eb88fc046392b49fb3ed6562103c1d308
urmisaha <urmisaha.88@gmail.com>
2018-03-22 12:16:10 +0530

    another sample test file added with all passed test cases to see changes in the allure dashboard

--- all_correct.py
+++ all_correct.py
@@ -1,12 +1,12 @@
 # content of test_sample.py
 def func(x):
+    return x + 1
+
+def func1(y):
     return x - 1
 
 def test_correct_answer():
     assert func(3) == 4
 
-def test_wrong_answer():
-    assert func(5) == 
-4
-def test_wrong_answer2():
-    assert func(6) == 4
+def test_correct_answer1():
+    assert func1(5) == 4
