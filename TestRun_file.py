ddef2abf31703df3998fce97fdf294fcc4d3787e
urmisaha <urmisaha.88@gmail.com>
2018-03-26 14:46:27 +0530

    CC-selenium login test code check

--- TestRun_file.py
+++ TestRun_file.py
@@ -4,39 +4,21 @@ import unittest
 import LoginWrongCredentialsTest
 import LoginCorrectCredentialsTest
 import os
-#import HTMLTestRunner
-
-#direct = os.getcwd() + "\HTMLRunner"
 
 class MyTestSuite(unittest.TestCase):
 
-	def test_Issue1(self):
-		login_test = unittest.TestSuite()
-		login_test.addTests([
-			unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect),
-			#unittest.defaultTestLoader.loadTestsFromTestCase(LoginWrongCredentialsTest.LoginWrong),
-		])
+	def test_login_correct_credential(self):
+		unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect)
+		# login_test = unittest.TestSuite()
+		# login_test.addTests([
+		# 	unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect),
+		# ])
 
-	def test_Issue2(self):
+	def test_login_wrong_credential(self):
 		login_test = unittest.TestSuite()
 		login_test.addTests([
-			#unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect),
 			unittest.defaultTestLoader.loadTestsFromTestCase(LoginWrongCredentialsTest.LoginWrong),
 		])
-
-		# outfile = open("TestReport.html", "w")
-
-		# runner1 = HTMLTestRunner.HTMLTestRunner(
-		# 	stream=outfile,
-		# 	title='Test Report',
-		# 	description='Login Tests'
-		# )
-
-		# runner1.run(login_test)
-		# outfile.close()
-
-
-
 
 
 if __name__ == '__main__':
