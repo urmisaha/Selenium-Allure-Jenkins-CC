80618cd3fc1af678942478870f4ead2136e22180
urmisaha <urmisaha.88@gmail.com>
2018-03-26 14:52:52 +0530

    CC-selenium login test code check and file name changes

--- Test_login.py
+++ Test_login.py
@@ -1,5 +1,5 @@
 __author__ = 'urmi and minali'
-# no change
+
 import unittest
 import LoginWrongCredentialsTest
 import LoginCorrectCredentialsTest
@@ -9,16 +9,9 @@ class MyTestSuite(unittest.TestCase):
 
 	def test_login_correct_credential(self):
 		unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect)
-		# login_test = unittest.TestSuite()
-		# login_test.addTests([
-		# 	unittest.defaultTestLoader.loadTestsFromTestCase(LoginCorrectCredentialsTest.LoginCorrect),
-		# ])
 
 	def test_login_wrong_credential(self):
-		login_test = unittest.TestSuite()
-		login_test.addTests([
-			unittest.defaultTestLoader.loadTestsFromTestCase(LoginWrongCredentialsTest.LoginWrong),
-		])
+		unittest.defaultTestLoader.loadTestsFromTestCase(LoginWrongCredentialsTest.LoginWrong)
 
 
 if __name__ == '__main__':
